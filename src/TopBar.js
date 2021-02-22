4e526a90c3da7488e651171be1a90e109b6b6d65
ja sam <asd>
2021-02-22 11:46:44 +0100

    initial commit

---
+++ src/TopBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TopBar = () => {
+    return (
+        <span className="topbar">
+                <a className="name" href="./App.js">ELEMENT</a>
+                <a className="about" href=".">About</a>
+                <a className="cart" href=".">Cart</a>
+        </span>
+    )
+}
+
+export default TopBar;
---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <span className='header'>
+            <img className="photo1" src="./photo1.jpg" alt="women" />
+            <img className="photo2" src="./photo2.jpg" alt="ni-" />
+        </span>
+    )
+}
+
+export default Header;
