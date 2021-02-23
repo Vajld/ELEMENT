4597bbf312bec2ae394ad467e701e96b7cda51f0
Aleksandar Hough <45799937+Eous638@users.noreply.github.com>
2021-02-23 11:04:34 +0100

    milan cant import lmao

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './header'
-import TopBar from './TopBar'
+import Header from './components/header'
+import TopBar from './components/TopBar'
 
 const App = () => {
   return (
