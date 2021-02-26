bb5b93392b92e893febbb548bbcac92212260a30
ja sam <asd>
2021-02-26 02:11:04 +0100

    added a photo layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import photo1 from '../assets/photo1.jpg'
-import photo2 from '../assets/photo2.jpg'
+import photo1 from '../assets/img/photo1.jpg'
+import photo2 from '../assets/img/photo2.jpg'
+
 const Header = () => {
     return (
         <div className='header'>
-            <img classname='photo1' src={photo1} />
-            <img className='photo2' src={photo2} />
+            <img className='photo1' src={photo1} alt="stol" />
+            <img className='photo2' src={photo2} alt="stol" />
         </div>
     )
 }
